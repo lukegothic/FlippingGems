4faba6afc71ae9dc9ac1e148de7190ea58cb6fd2
xabixab <xabixab@gmail.com>
2017-01-21 21:07:10 +0100

    add test mode

---
+++ test.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use('/vendor', express.static('bower_components'));
+app.use('/', express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
