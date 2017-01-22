78069a3eb6122ab65ffe2c1421db69fdfe40312e
Iván Pérez Gómez <Ivn Prez Gmez>
2017-01-22 07:07:22 +0100

    version en sin servidor

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,136 @@
-
+Game = window.Game || {};
+Game.boardSize = 6;
+Game.PLAYERNUM = {
+    NONE: 0,
+    P1: 1,
+    P2: 2
+};
+Game.board = [];
+Game.currentPlayer = null;
+Game.displayPlayer = function(player) {
+    player = player || this.currentPlayer;
+    $("body").removeClass().addClass("player" + this.currentPlayer);
+};
+Game.nextPlayer = function() {
+    switch (this.currentPlayer) {
+        case 1:
+            this.currentPlayer = this.PLAYERNUM.P2;
+        break;
+        case 2:
+            this.currentPlayer = this.PLAYERNUM.P1;
+        break;
+        default:
+        this.currentPlayer = Math.round(Math.random()) + 1;
+        break;
+    }
+    this.displayPlayer();
+};
+Game.getScores = function() {
+    var scores = [0, 0, 0];
+    for (var i = 0; i < this.board.length; i++) {
+        for (var j = 0; j < this.board[i].length; j++) {
+            scores[this.board[i][j].who]++;
+        }
+    }
+    return scores;
+};
+Game.updateScore = function(scores) {
+    scores = scores || this.getScores();
+    $("#p1score").html(scores[this.PLAYERNUM.P1]);
+    $("#p2score").html(scores[this.PLAYERNUM.P2]);
+};
+Game.getAdjacentSquares = function(position) {
+    var squares = [];
+    var x,y;
+    // arriba
+    x = position.x;
+    y = position.y - 1;
+    if (y >= 0 && y < Game.boardSize) {
+        squares.push({ x: x, y: y });
+    }
+    // abajo
+    y = position.y + 1;
+    if (y >= 0 && y < Game.boardSize) {
+        squares.push({ x: x, y: y });
+    }
+    // izda
+    x = position.x - 1;
+    y = position.y;
+    if (x >= 0 && x < Game.boardSize) {
+        squares.push({ x: x, y: y });
+    }
+    // derecha
+    x = position.x + 1;
+    if (x >= 0 && x < Game.boardSize) {
+        squares.push({ x: x, y: y });
+    }
+    return squares;
+};
+Game.newGame = function() {
+    $("#end").hide();
+    this.currentPlayer = null;
+    this.board = [];
+    $(".board").empty();
+    var rowElm, squareElm;
+    for (var i = 0; i < this.boardSize; i++) {
+        this.board[i] = [];
+        rowElm = $("<div>").addClass("boardrow");
+        for (var j = 0; j < this.boardSize; j++) {
+            this.board[i][j] = {
+                who: this.PLAYERNUM.NONE,
+                elem: $("<div>").addClass("square player" + this.PLAYERNUM.NONE).data({ x: i, y: j})
+            };
+            rowElm.append(this.board[i][j].elem);
+        }
+        $(".board").append(rowElm);
+    }
+    this.updateScore();
+    this.nextPlayer();
+};
+Game.endGame = function (scores) {
+    var winner = $("#winner");
+    if (scores[this.PLAYERNUM.P1] > scores[this.PLAYERNUM.P2]) {
+        winner.html("GANA EL ROJO");
+        this.displayPlayer(this.PLAYERNUM.P1);
+    } else if (scores[this.PLAYERNUM.P1] < scores[this.PLAYERNUM.P2]) {
+        winner.html("GANA EL AZUL");
+        this.displayPlayer(this.PLAYERNUM.P2);
+    } else {
+        winner.html("EMPATE");
+        this.displayPlayer(this.PLAYERNUM.NONE);
+    }
+    $("#end").show();
+};
+Game.init = function() {
+    $(".board").on("click", ".square", function() {
+        var square = $(this);
+        var squareInfo = square.data();
+        if (Game.board[squareInfo.x][squareInfo.y].who == Game.PLAYERNUM.NONE) {
+            Game.board[squareInfo.x][squareInfo.y].who = Game.currentPlayer;
+            square.addClass("player" + Game.currentPlayer);
+            var adjacentSquares = Game.getAdjacentSquares(squareInfo);
+            var theSquare;
+            for (var i = 0; i < adjacentSquares.length; i++) {
+                theSquare = Game.board[adjacentSquares[i].x][adjacentSquares[i].y];
+                if (theSquare.who !== Game.currentPlayer && theSquare.who !== Game.PLAYERNUM.NONE) {
+                    theSquare.who = Game.currentPlayer;
+                    theSquare.elem.removeClass().addClass("square player" + Game.currentPlayer);
+                }
+            }
+            var scores = Game.getScores();
+            Game.updateScore(status);
+            if (scores[Game.PLAYERNUM.NONE] == 0) {
+                Game.endGame(scores);
+            } else {
+                Game.nextPlayer();
+            }
+        } else {
+            console.log("casilla ocupada por " + Game.board[squareInfo.x][squareInfo.y].who);
+        }
+    });
+    $("#newGame").click(function() {
+        Game.newGame();
+    });
+    this.newGame();
+};
+Game.init();
